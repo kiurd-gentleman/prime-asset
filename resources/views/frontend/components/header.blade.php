e277291d033261b592aa5528221d34ea74a6003c
kiurd-gentelman <65729806+kiurd-gentleman@users.noreply.github.com>
2024-09-10 00:54:18 +0600

    asset and template added

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -1,2 +1,67 @@
+<div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>
+<!-- Spinner End -->
 
-<h1>My Laravel App Header</h1>
+
+<!-- Navbar Start -->
+<div class="container-fluid nav-bar bg-transparent">
+    <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
+        <a href="index.html" class="navbar-brand d-flex align-items-center text-center">
+            <div class="icon p-2 me-2">
+                <img class="img-fluid" src="{{asset('/frontend/customimages/logo.png')}}" alt="Icon" style="height: 30px;">
+            </div>
+        </a>
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <a href="about.html" class="nav-item nav-link">Projects</a>
+                <a href="about.html" class="nav-item nav-link">Apartments</a>
+                <a href="about.html" class="nav-item nav-link">Concerns</a>
+                <a href="about.html" class="nav-item nav-link">Consumer</a>
+                <a href="about.html" class="nav-item nav-link">Gallery</a>
+                <!-- <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Property</a>
+                    <div class="dropdown-menu rounded-0 m-0">
+                        <a href="property-list.html" class="dropdown-item">Property List</a>
+                        <a href="property-type.html" class="dropdown-item">Property Type</a>
+                        <a href="property-agent.html" class="dropdown-item">Property Agent</a>
+                    </div>
+                </div> -->
+
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <a href="" class="btn btn-primary px-3 d-none d-lg-flex">Download App</a>
+        </div>
+    </nav>
+</div>
+<!-- Navbar End -->
+
+
+<!-- Header Start -->
+<div class="container-fluid header bg-white p-0">
+    <div class="row g-0 align-items-center flex-column-reverse flex-md-row">
+        <div class="col-md-6 p-5 mt-lg-5">
+            <!-- <h1 class="display-5 animated fadeIn mb-4">Find A <span class="text-primary">Perfect Home</span> To Live With Your Family</h1> -->
+            <h1 class="display-5 animated fadeIn mb-4"> Condominium & <span class="text-primary">Apartment </span> Project</h1>
+            <p class="animated fadeIn mb-4 pb-2">A modern township designed on modern sustainable principles of equity, livability, and competitiveness</p>
+            <a href="" class="btn btn-primary py-3 px-5 me-3 animated fadeIn">Get a quote</a>
+        </div>
+        <div class="col-md-6 animated fadeIn">
+            <div class="owl-carousel header-carousel">
+                <div class="owl-carousel-item">
+                    <img class="img-fluid" src="{{asset('/frontend/img/carousel-1.jpg')}}" alt="">
+                </div>
+                <div class="owl-carousel-item">
+                    <img class="img-fluid" src="{{asset('/frontend/img/carousel-2.jpg')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -1,30 +1,71 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
-    <title>Document</title>
-    <style>
-        body {
-            font-family: 'figtree', sans-serif;
-        }
-    </style>
+    <meta charset="utf-8">
+    <title>Prime Asset Group</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@700;800&display=swap"
+        rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{asset('/frontend/lib/animate/animate.min.css')}}" rel="stylesheet">
+    <link href="{{asset('/frontend/lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{asset('/frontend/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="{{asset('/frontend/css/style.css')}}" rel="stylesheet">
     @yield('styles')
 </head>
+
 <body>
-<header>
-    @include('frontend.components.header')
-</header>
-<main>
-    @yield('content')
-</main>
-<footer>
-   @include('frontend.components.footer')
-</footer>
-@yield('scripts')
+    <div class="bg-white p-0">
+        <!-- Header Start -->
+
+        @include('frontend.components.header')
+        <!-- Header End -->
+
+        <main>
+            @yield('content')
+        </main>
+
+
+
+
+        <!-- Footer Start -->
+        @include('frontend.components.footer')
+        <!-- Footer End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+    @yield('scripts')
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('/frontend/lib/wow/wow.min.js')}}"></script>
+    <script src="{{asset('/frontend/lib/easing/easing.min.js')}}"></script>
+    <script src="{{asset('/frontend/lib/waypoints/waypoints.min.js')}}"></script>
+    <script src="{{asset('/frontend/lib/owlcarousel/owl.carousel.min.js')}}"></script>
+
+    <!-- Template Javascript -->
+    <script src="{{asset('/frontend/js/main.js')}}"></script>
 </body>
+
 </html>
